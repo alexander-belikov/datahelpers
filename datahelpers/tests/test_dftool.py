eac71b0858cc71900abd22c7b9b32258190618b5
Alexander Belikov <abelikov@gmail.com>
2017-02-02 20:10:40 -0600

    fix imports in test_dftool

--- datahelpers/tests/test_dftool.py
+++ datahelpers/tests/test_dftool.py
@@ -1,11 +1,11 @@
+from pandas import DataFrame
 import datahelpers.dftools as dt
-import pandas as pd
-import unittest
+from ..dftools import create_unique_index
+from unittest import TestCase, main
 
 
-class TestDFTolls(unittest.TestCase):
-    #TODO fix the test
-    df0 = pd.DataFrame({'i1': [0, 1, 2, 2, 2, 3, 4, 5, 5],
+class TestDFTolls(TestCase):
+    df0 = DataFrame({'i1': [0, 1, 2, 2, 2, 3, 4, 5, 5],
                         'i2': [0, 0, 1, 2, 3, 4, 5, 5, 6],
                         'i3': ['a']*9})
 
@@ -19,7 +19,7 @@ class TestDFTolls(unittest.TestCase):
 
     def test_create_unique_index_not_sni(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'])
+        dfr = create_unique_index(self.df0, ['i1', 'i2'])
         self.assertListEqual(list(dfr['i1xi2'].values), self.not_sni)
 
     def test_create_unique_index_i1_sni(self):
@@ -29,29 +29,28 @@ class TestDFTolls(unittest.TestCase):
 
     def test_create_unique_index_i1_sni_ambi_i1(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'], ['i1'], 'i1')
+        dfr = create_unique_index(self.df0, ['i1', 'i2'], ['i1'], 'i1')
         self.assertListEqual(list(dfr['i1xi2'].values), self.i1_sni_ambi_i1)
-
 
     def test_create_unique_index_i1_sni_ambi_i2(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'], ['i1'], 'i2')
+        dfr = create_unique_index(self.df0, ['i1', 'i2'], ['i1'], 'i2')
         self.assertListEqual(list(dfr['i1xi2'].values), self.i1_sni_ambi_i2)
 
     def test_create_unique_index_i2_sni(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'], ['i2'])
+        dfr = create_unique_index(self.df0, ['i1', 'i2'], ['i2'])
         self.assertListEqual(list(dfr['i1xi2'].values), self.i2_sni)
 
     def test_create_unique_index_i2_sni_ambi_i1(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'], ['i2'], 'i1')
+        dfr = create_unique_index(self.df0, ['i1', 'i2'], ['i2'], 'i1')
         self.assertListEqual(list(dfr['i1xi2'].values), self.i2_sni_ambi_i1)
 
     def test_create_unique_index_i2_sni_ambi_i2(self):
 
-        dfr = dt.create_unique_index(self.df0, ['i1', 'i2'], ['i2'], 'i2')
+        dfr = create_unique_index(self.df0, ['i1', 'i2'], ['i2'], 'i2')
         self.assertListEqual(list(dfr['i1xi2'].values), self.i2_sni_ambi_i2)
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
